05c330e3675e3d2e8cdd536ae007c574014c351f
albertchai01 <albertchai@chaisystems.net>
2017-10-06 13:19:42 -0700

    Weight Edge List
    updated weight edge list and files

--- main.R
+++ main.R
@@ -1,5 +1,8 @@
 # UCSD Lo Lab Group Social Network Analysis Script
 
+# Loads Junk Variable
+gcheckfalse = 0
+
 # load the library
 library(igraph)
 
@@ -12,7 +15,8 @@ g <- graph.data.frame(importedData, directed = outcomeBool) # Gets edgelist
 
 # import weight list; run as Source with Echo to get user input
 userInputweightaccept <- readline("Do you have a weighted list? Please enter 1 for TRUE or 0 for FALSE\t")
-ifelse(userInputweightaccept ==1, g_weight <- importedData_weight <- read.csv(file.choose(), header = TRUE), next(userInputweightaccept==0))
+ifelse(userInputweightaccept ==1, g_weight <- importedData_weight <- read.csv(file.choose(), header = TRUE), gcheckfalse == 0)
+E(g)$weight <- sample(seq_len(ecount(g)))
 
 # Creates and graphs weighted adjacency matrix from edgelist
 ifelse(userInputdir == 1, outcome <- "directed", outcome <- "undirected")
@@ -28,12 +32,12 @@ nedge <- ecount(g) # Edge count
 nnode <- gorder(g) # Node count
 den <- edge_density(g, loops=TRUE) # Density with self interactions allowed
 inoutdeg <- degree(g) # Degrees of all nodes
-degavg <- mean(degree(g)) # Average degree
-diam <- diameter(g) # Diameter of graph
+degavg <- mean(degree(g), weights = g_weight) # Average degree
+diam <- diameter(g, weights = g_weight) # Diameter of graph
 
 strwkplotchk <- clusters(g, mode = "strong")$membership # Finding and plotting strong/weak clusters
 plot(g, vertex.color = strwkplotchk, edge.curved = FALSE)
-
+ 
 membershipvec <- cluster_walktrap(g) # Creates a vector based on short walkthroughs to find communities
 submod <- modularity(membershipvec) # Finds unrestricted modularity of graph
 ifelse(userInputdir == 0, relsubmod, stop(userInputdir == 0)) # Checks if modularity can be calculated based on graph directionality
