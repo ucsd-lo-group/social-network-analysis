213b6ce26f89390426775b84b6ac64d61aea40ac
albertchai01 <albertchai@chaisystems.net>
2017-10-08 23:22:53 -0700

    weight function updated
    updated weights function, reorganized script based on sections, fixed adj matrix based on weight. Graph plot based on weights, density, diamter still need to be fixed

--- main.R
+++ main.R
@@ -1,29 +1,31 @@
+#####################################################################################################################
 # UCSD Lo Lab Group Social Network Analysis Script
 # Run script as Source with Echo in RStudio for user input process, else script will fail
+# MIT License
+# Written by Albert Chai and Joshua Pei Le
+# Principal Investigator: Stanley M. Lo
 
+#####################################################################################################################
 # Loads Junk Variable
 gcheckfalse = 0
 
+#####################################################################################################################
 ## Checks for Pre-requisite Libraries
 library(igraph)
 #library(network)
 #library(sna)
 
+#####################################################################################################################
 ## Data Import
 # Asks user to seelct the data to import
-#importedData_mod <- read.csv(file.choose(), header = TRUE)
 importedData <- read.csv(file.choose(), header = TRUE)
 
 # Promopts user to state if data is directed and confirm with 1 or 0
 userInputdir <- readline("Is your data directed? Please enter 1 for TRUE or 0 for FALSE\t")
 ifelse(userInputdir == 1, outcomeBool <- "TRUE", outcomeBool <- "FALSE")
 
-# Converts user input list to numeric value
-#importedData <- lapply(importedData_mod, as.numeric)
-
 # Imports data based on directionality
 g <- graph.data.frame(importedData, directed = outcomeBool) 
-
 
 # Asks user if there is a weight for the imported data list
 userInputweightaccept <- readline("Do you have a weighted list? Please enter 1 for TRUE or 0 for FALSE\t")
@@ -33,44 +35,55 @@ userInputweightaccept <- readline("Do you have a weighted list? Please enter 1 f
 ifelse(userInputweightaccept ==1, importedData_weight <- read.csv(file.choose(), header = TRUE), gcheckfalse == 0)
 
 # Converts user input list to numeric value
-g_weight <- lapply(importedData_weight, as.numeric)
+g_weight <- unlist(importedData_weight)
 
 # Creates an attribute for weight
 E(g)$weight <- g_weight
-#E(g)$weight <- g
+
+# Confirms that the graph is weighted
+graph_weighted <- ifelse(userInputweightaccept == 1, is.weighted(g), gcheckfalse == 0)
 
 # Creates Graph Adjacency Matrix
 graphadj <- get.adjacency(g)
 get.diameter(g, directed = userInputdir)
 ifelse(userInputdir == 1, outcome <- "directed", outcome <- "undirected")
-gadj <- get.adjacency(g, sparse=FALSE) # Gets weighted adjacency matrix
+
+# Gets weighted adjacency matrix
+gadj <- get.adjacency(g, edges = TRUE, sparse = TRUE) 
 graphedadj <- graph.adjacency(gadj, mode = outcome, weighted = TRUE)
 
 # Creates Plot of Social Network Graph
-plot(graphedadj) # WORK IN PROGRESS
+plot_raw <- plot(graphedadj) # WORK IN PROGRESS - GRAPH WITH WEIGHTS
 
 # plot the data using straight lines (curved algorithm causes warnings)
-#plot(g, edge.curved = FALSE)
+plot_raw_straight <- plot(g, edge.curved = FALSE)
 
+#####################################################################################################################
 ## Core Basic Analysis Parameters
-nedge <- ecount(g) # Edge count
-nnode <- gorder(g) # Node count
-den <- edge_density(g, loops=TRUE) # Density with self interactions allowed
-inoutdeg <- degree(g) # Degrees of all nodes
-degavg.directional <- mean(degree(g)) # Average degree (both directions)
-degavg <- (mean(degree(g))/2) # Average degree based on first occurance
-
-## WORK IN PROGESS, ERROR RESULTS IN WEIGHT LIST CANNOT BE COERCED TO TYPE 'DOUBLE'
-diam <- diameter(g, directed = userInputdir) # Diameter of graph
-
-strwkplotchk <- clusters(g, mode = "strong")$membership # Finding and plotting strong/weak clusters
-plot(g, vertex.color = strwkplotchk, edge.curved = FALSE)
+# Edge Count
+nedge <- ecount(g)
+# Node Count
+nnode <- gorder(g)
+# Density with self interactions allowed
+den <- edge_density(g, loops=TRUE) 
+# Degrees of all nodes
+inoutdeg <- degree(g) 
+# Average degree (both directions)
+degavg.directional <- mean(degree(g)) 
+# Average degree based on first occurance
+degavg <- (mean(degree(g))/2) 
+# Diameter of graph
+diam <- diameter(g, directed = userInputdir) 
+# Finding and plotting strong/weak clusters
+strwkplotchk <- clusters(g, mode = "strong")$membership 
+plot_cluster <- plot(g, vertex.color = strwkplotchk, edge.curved = FALSE)
  
 membershipvec <- cluster_walktrap(g) # Creates a vector based on short walkthroughs to find communities
 submod <- modularity(membershipvec) # Finds unrestricted modularity of graph
 ifelse(userInputdir == 0, relsubmod, stop(userInputdir == 0)) # Checks if modularity can be calculated based on graph directionality
 relsubmod <- modularity(g, membership(membershipvec), weights = NULL) # Finds modularity relative to membershipvec, a vector of communities
 
+#####################################################################################################################
 ## Centrality of Network Members
 #central <- centralize()
 # Centrality based on Eigenvector Centrality
