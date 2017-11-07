09d8e000c041114ee3b2037f36d960c8df11d540
Andrew Lee <alee4738@gmail.com>
2017-11-07 14:24:40 -0800

    Fix number of active participants + small changes
    move section "Question and Response Matrix Creation" above "Number of participants in the group"
    rename variable amax to num_participants_active
    rename variable gmax to num_participants_total
    rename variable nop to num_participants_inactive
    remove variable nopinitial
    simplify non-participating member prompts
    'your value' and 'your new value' are ambiguous, changed to number of inactive participants and total number of participants

--- rawdatamatrixprocessor.m
+++ rawdatamatrixprocessor.m
@@ -44,45 +44,6 @@ userInput = input('File: ');
 [q,r] = importfile(userInput);
 disp('Data import has been completed...')
 
-%% Maximum number of participants in the group
-% qmax and rmax determines the highest number of participants in their respective columns.
-qmax = max(q);
-rmax = max(r);
-% amax determines the highest number of participants in the group from each of the question and response group
-amax = max(qmax,rmax);
-disp('Calculated Maximum Participants...')
-fprintf('STATUS: The total number of ACTIVE participants is %d. \n',amax)
-% There are posssibilities that there are non-participants in the group, this section will ask user for the input
-% and continue with calculations based on new value
-disp(' ')
-disp('Are there any non-participants in the group? Please enter the number of non-interacting participants or else enter 0.')
-disp('Use zero or postive integers only, any decimal value will be rounded!!!')
-nopinitial = input('Number of non-participating members: ');
-nop = round(nopinitial);
-% Code uses a negative while check, rather than postive condition check, to determine whether a user enters a valid value.
-while nop < 0
-    nop = input('Sorry, you entered an invalid value, please enter a valid value: ');
-    % If user fails to enter a valid number greater than or equal to 0, the script will reprompt until a valid value has been entered.
-end
-% Determines new Max number of participants
-gmax = amax + nop; 
-% Asks user to confirm whether the new value is correct. If value is not correct, script will terminate.
-fprintf('STATUS: You have entered %d, as your value. \n', nopinitial)
-fprintf('STATUS: If the value was not an integer, it has been automatically rounded for you. Your new non-participant value is: %d. \n', nop)
-fprintf('STATUS: Your new value is %d. Please note that this value will be used for necessary calculations. \n', gmax)
-maxconfirm=input('Confirm by entering 1 if the value is correct: ');
-    if maxconfirm == 1
-        disp('Processing...')
-    else
-        disp('STATUS: The script has been paused, stop the script before continuing. It is recommended that you clear your workspace.')
-        disp('STATUS: Please re-run this section and re-enter the proper value.')
-        disp('STATUS: Press CTRL+C for Windows to terminate the script')
-        pause      
-end
-% gmax is the number of total participants in the group
-% amax is the number of ACTIVE participants in the group
-% nop is the number of non-participants that the user will enter to the script
-
 %% Question and Response Matrix Creation
 % vs is one matrix containing both q and r data
 vs = [q, r];
@@ -91,6 +52,41 @@ q0 = fillmissing(q,'constant',0);
 r0 = fillmissing(r,'constant',0);
 % vall function will process all the question and response data into one column in order of sequence
 vall = q0+r0;
+
+%% Number of participants in the group
+% num_participants_active is the number of active participants in the group (both q and r)
+num_participants_active = size(unique(vall(vall ~= 0)),1);
+disp('Calculated Number of Active Participants...')
+fprintf('STATUS: The total number of ACTIVE participants is %d. \n',num_participants_active)
+
+% prompt user for possible non-participants
+disp(' ')
+num_participants_inactive = round(input('Number of non-participating members (integer >= 0, decimals are rounded): '));
+% number of inactive participants cannot be negative
+while num_participants_inactive < 0
+    num_participants_inactive = input('Sorry, you entered an invalid value, please enter a valid value: ');
+    % If user fails to enter a valid number greater than or equal to 0, the script will reprompt until a valid value has been entered.
+end
+
+% num_participants_total is the number of total participants
+num_participants_total = num_participants_active + num_participants_inactive; 
+
+% Confirm total number of participants: If value is not correct, script will terminate.
+fprintf('STATUS: You have entered %d, as number of non-participating members. \n', num_participants_inactive)
+fprintf('STATUS: If the value was not an integer, it has been automatically rounded for you. \n')
+fprintf('STATUS: Your total number of participants is %d. Please note that this value will be used for necessary calculations. \n', num_participants_total)
+maxconfirm=input('Confirm by entering 1 if the value is correct: ');
+    if maxconfirm == 1
+        disp('Processing...')
+    else
+        disp('STATUS: The script has been paused, stop the script before continuing. It is recommended that you clear your workspace.')
+        disp('STATUS: Please re-run this section and re-enter the proper value.')
+        disp('STATUS: Press CTRL+C for Windows to terminate the script')
+        pause
+    end
+% num_participants_total is the number of total participants in the group
+% num_participants_active is the number of ACTIVE participants in the group
+% num_participants_inactive is the number of non-participants that the user will enter to the script
 
 %% Total number of interactions 
 % intot determines the total number of interactions the members in the group has. Based on the vall matrix grid.
@@ -111,24 +107,24 @@ disp('Discussion order has been created.')
 %% Student Matrix Interactions
 % Determines the number of times students interact with one another in a directed manner
 % Creates a matrix of the total number of active students
-serialact = 1:amax;
+serialact = 1:num_participants_active;
 % Transposes serialact into a vertical matrix
 serialact1 = serialact';
 % Creates a matrix of the total number of students
-serialtot = 1:gmax;
+serialtot = 1:num_participants_total;
 % Transposes serialtot into a vertical matrix
 serialtot1 = serialtot';
-% Begins creating first variable loop for active participants, amax is used.
+% Begins creating first variable loop for active participants, num_participants_active is used.
     % A vector with the desired length of all possible combinations is preallocated with prealloc, values are zeros
     disp('Script is preallocating vector size for all possible combinations...')
-    prealloc = zeros(amax*amax,2);
+    prealloc = zeros(num_participants_active*num_participants_active,2);
     disp('Preallocation is complete')
     
 % Indexing values of matrix for statistical analysis of data
 disp('Values are being populated...')
-for i = 1: 1: amax % i = 1, then 2, 3, 4, ... then lastly amax
-	for j = 1: 1: amax % j = 1, then 2, 3, 4, ... then lastly amax
-    row_num = (i-1)*amax + j; 
+for i = 1: 1: num_participants_active % i = 1, then 2, 3, 4, ... then lastly num_participants_active
+	for j = 1: 1: num_participants_active % j = 1, then 2, 3, 4, ... then lastly num_participants_active
+    row_num = (i-1)*num_participants_active + j; 
     prealloc(row_num,1) = i;
     prealloc(row_num,2) = j;
 	end
