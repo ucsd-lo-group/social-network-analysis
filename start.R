e621c33a1e060c1435e7289c43056090e0da3189
Albert Chai <achai@r.chai.systems>
2018-01-28 09:32:54 +0000

    Renamed files and created starter script

---
+++ start.R
@@ -0,0 +1,12 @@
+######### Set Workspace Working Directory #########
+checkwdset <- readline('Is your working directory set to the correct directory with all dependent files? 1 for YES or 0 for NO: ')
+if(checkwdset == 0) #If user indicates setwd is not correct, prompts user to change wd to correct location
+{
+  cat('Please indicate the working directory of the social network analysis files')
+  cat("Format: ~/path/to/social-network-analysis\n")
+  setwdvar <- readline('Working Directory of Script files: ')
+  setwd(setwdvar)
+}
+
+######### Disables ECHO for all scripts, run for clean interface #########
+source('source.R',echo = FALSE)
