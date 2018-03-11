5ee9a457d17694686a34773ea3dcd3b93f242b0c
albertchai01 <albertchai@chaisystems.net>
2018-03-11 15:28:01 -0700

    Adds Directory Checks
    Checks if the user is in the correct working directory before proceeding to the next step.

---
+++ setdircheck.R
@@ -0,0 +1,3 @@
+# Script checks if the directory is properly set
+cat('Directory has been set successfully \n')
+cat('\n')
--- start.R
+++ start.R
@@ -1,5 +1,18 @@
 # Start R script
 # Set working directory and execute based on dependencies file
+# Check user directory
+cat('Your current working directory is:\n')
+getwd() # Gets current working directory
+checkwdloc <- readline('Please confirm this is the root directory of the script [1 for yes or [ENTER]/0 for no]: \t')
+if(checkwdloc == 0)
+{
+  # Asks user to define current directory of files
+  getscriptdir <- readline('Enter your directory of the script files (No quotes): \t')
+  setwd(getscriptdir) #Sets working directory based on user input
+}
+
+# Executes Directory Verification Script
+source('setdircheck.R',echo = FALSE)
 
 # Code Version Check
 # Sets current script version for Info Print Out
