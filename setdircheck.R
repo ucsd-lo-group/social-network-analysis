fc14549842c3f26ffd45ea23449737b472148815
Albert Chai <albertchai@chaisystems.net>
2018-10-04 19:13:38 -0700

    Bug fixes

---
+++ setdircheck.R
@@ -0,0 +1,9 @@
+# Script checks if the directory is properly set
+# UCSD Lo Lab Group Social Network Analysis Script
+# MIT License
+# Written by Albert Chai, Andrew S. Lee and Joshua P. Le
+
+cat('Directory has been set successfully \n')
+cat('\n')
+
+dirsettrue <- 1
